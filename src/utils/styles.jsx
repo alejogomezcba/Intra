18c0af84ccb9e1d948eecc52b5a7b62b849d20ab
Alejo Gomez <alejorgomez@hotmail.com>
2025-02-03 16:55:40 -0300

    Add files via upload

---
+++ src/utils/styles.jsx
@@ -0,0 +1,9 @@
+export const globalColors = {
+    green: "#36c792",
+    yellow: "#f7df63",
+    red: "#ff3363",
+    blue: "#00A3E4",
+    gray: "#f2f2f2",
+    grayTwo: "#a2a2a2",
+    colorForOpacity: "rgba(0, 163, 228",
+}
